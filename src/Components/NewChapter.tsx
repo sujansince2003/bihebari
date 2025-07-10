e3faa949937fc40e50a964e9b30631f1a5210eb2
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-07-11 01:04:18 +0545

    built new chapter component

---
+++ src/Components/NewChapter.tsx
@@ -0,0 +1,14 @@
+const NewChapter = () => {
+  return (
+    <div className="bg-[#0248ACB2] py-10 flex justify-center items-center gap-10">
+      <h1 className="text-3xl text-white">
+        Your new chapter is just a click away.
+      </h1>
+      <button className="border font-normal py-3 px-6 rounded-md border-amber-50 text-white">
+        Get Started
+      </button>
+    </div>
+  );
+};
+
+export default NewChapter;
