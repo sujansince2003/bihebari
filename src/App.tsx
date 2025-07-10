74f0895d765dd765ca2ed8bdbf1e229c6400cb4d
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-07-10 08:21:43 +0545

    setup react router dom

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Layout from "./Layout/Layout";
+import { Register, Login, Landing } from "./Pages";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Landing />
+            </Layout>
+          }
+        />
+        <Route path="/home" element={<Layout>Home</Layout>} />
+        <Route path="/about" element={<Layout>About</Layout>} />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
