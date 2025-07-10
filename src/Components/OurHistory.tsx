74068dc6cf1ca334601f3610142989d02b528133
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-07-10 08:54:35 +0545

    our history

---
+++ src/Components/OurHistory.tsx
@@ -0,0 +1,26 @@
+const OurHistory = () => {
+  return (
+    <div className="px-8 py-52 flex flex-col items-center gap-6">
+      <div>
+        {" "}
+        <h2 className="text-md text-center">About us</h2>
+        <h3 className="text-5xl">Our History</h3>
+      </div>
+
+      <p className="text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat.
+      </p>
+      <button className="bg-[#0066FF] text-white p-3 rounded-2xl font-medium text-[22px]">
+        Read more
+      </button>
+    </div>
+  );
+};
+
+export default OurHistory;
--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,9 +1,13 @@
 import { Hero } from "../Components";
+import Banner from "../Components/Banner";
+import OurHistory from "../Components/OurHistory";
 
 const Landing = () => {
   return (
     <div>
       <Hero />
+      <OurHistory />
+      <Banner />
     </div>
   );
 };
