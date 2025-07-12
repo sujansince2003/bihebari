a56d19f8bdda31c2cca556e6eff516aff85f18dd
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-07-12 17:27:03 +0545

    rsponsive newchapter

--- src/Components/NewChapter.tsx
+++ src/Components/NewChapter.tsx
@@ -1,10 +1,10 @@
 const NewChapter = () => {
   return (
-    <div className="bg-[#0248ACB2] py-10 flex justify-center items-center gap-10">
-      <h1 className="text-3xl text-white">
+    <div className="bg-[#0248ACB2] py-10 px-4 flex flex-col md:flex-row justify-center items-center gap-6 md:gap-10 text-center md:text-left">
+      <h1 className="text-xl md:text-3xl text-white md:max-w-full max-w-md">
         Your new chapter is just a click away.
       </h1>
-      <button className="border font-normal py-3 px-6 rounded-md border-amber-50 text-white">
+      <button className="border font-normal py-3 px-6 rounded-md border-amber-50 text-white hover:bg-white hover:text-[#0248AC] transition">
         Get Started
       </button>
     </div>
