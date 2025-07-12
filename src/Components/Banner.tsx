661ec3a61b42ed7cce9eecfc2e8d7c340c9dcd44
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-07-12 17:26:26 +0545

    change to responsive

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,16 +1,18 @@
 const Banner = () => {
   return (
     <div
-      className="relative py-[160px]  w-full  bg-cover bg-center bg-no-repeat"
+      className="relative w-full py-24 md:py-[160px] bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: "url('https://i.postimg.cc/bJjYN6F8/bannerimg.png')",
       }}
     >
-      <div className="flex flex-col items-center">
-        <p className="text-white font-dancingScript text-[150px] font-bold ">
+      <div className="flex flex-col items-center px-4 text-center">
+        <p className="text-white font-dancingScript text-[48px] sm:text-[80px] md:text-[120px] lg:text-[150px] font-bold leading-none">
           Bihebari
         </p>
-        <p className=" text-white  text-2xl ">Connecting Hearts with Care</p>
+        <p className="text-white text-base sm:text-lg md:text-2xl mt-2">
+          Connecting Hearts with Care
+        </p>
       </div>
     </div>
   );
